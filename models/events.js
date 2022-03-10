3b9817b0c87f25d192bf25880725513bf3365081
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2022-03-11 00:39:25 +0100

    Update events.js

--- models/events.js
+++ models/events.js
@@ -3,16 +3,17 @@ const mongoose = require("mongoose");
 const EventSchema = new mongoose.Schema({
 	title: { type: String, required: false },
 	about: { type: String, required: false },
-	attendees: { type: String, required: true },
 	date: { type: Date, required: true },
 	time: { type: String, required: true },
 	location: { type: String, required: true },
+	image: { type: String, required: true },
+	host: { type: String, required: true },
 	tags_id: {
 		type: Array,
 		default: [],
 		ref: "tags"
 	},
-	// likes: { type: Number, default: 0 },
+	attendees: { type: Number, default: 0 },
 	comment_no: { type: Number, default: 0 }
 },
 	{ timestamps: true });
