d18da7d859e15a327aadaba4319fe692400dd29c
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2022-03-15 14:15:21 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const app = express.Router();
 require('./endpoints/user')(app);
 require('./endpoints/event')(app);
 require('./endpoints/comment')(app);
+require('./endpoints/attendees')(app);
 require('./endpoints/tag')(app);
 
 module.exports = app;
