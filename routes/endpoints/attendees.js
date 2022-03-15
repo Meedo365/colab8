ae39b6023a06b14acd474c62fd0188c878a9048b
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2022-03-15 14:13:32 +0100

    Create attendees.js

---
+++ routes/endpoints/attendees.js
@@ -0,0 +1,50 @@
+let Attending = require("../../models/attendees");
+
+
+let routes = (app) => {
+    // app.post("/create/comment", async (req, res) => {
+    app.post("/attend", async (req, res) => {
+        try {
+            let attend = new Attending(req.body);
+            await attend.save()
+            res.json(attend)
+        }
+        catch (err) {
+            res.status(500).send(err)
+        }
+    });
+
+    app.get("/attend", async (req, res) => {
+        try {
+            let attend = await Attending.find()
+                .populate("user_id")
+                .populate("event_id")
+            res.json(attend)
+        }
+        catch (err) {
+            res.status(500).send(err)
+        }
+    });
+
+    app.put("/attend/:id", async (req, res) => {
+        try {
+            let attend = await Attending.findOne({ _id: req.params.id });
+            res.json(attend)
+        }
+        catch (err) {
+            res.status(500).send(err)
+        }
+    });
+    
+     app.delete("/attend/:id", async (req, res) => {
+        try {
+            let attend = await Attending.deleteOne({ _id: req.params.id });
+            res.json(attend)
+        }
+        catch (err) {
+            res.status(500).send(err)
+        }
+    });
+}
+
+module.exports = routes;
