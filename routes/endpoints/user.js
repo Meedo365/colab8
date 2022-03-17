49d0b6b2fd72a173719e08d430be0861c3bf0a49
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2022-03-17 10:49:51 +0100

    Update user.js

--- routes/endpoints/user.js
+++ routes/endpoints/user.js
@@ -59,13 +59,13 @@ let routes = (app) => {
     app.put("/login", async (req, res) => {
         try {
             let { email, password } = req.body;
-            let active = true;
-            let user = await User.findOneAndUpdate({ email, password });
+            let update ={active: true};
+            let user = await User.findOneAndUpdate({ email, password }, update, {returnOriginal: false});
             if (user == null) {
                 return res.json({ msg: "Invalid email or password" })
             } else {
-                user.active = true;
-                await user.save()
+//                 user.active = true;
+//                 await user.save()
                 res.json(user)
             }
         }
