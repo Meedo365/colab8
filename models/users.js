12844e3b9e547b2a449339885ac055634b867c16
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2022-03-18 23:56:03 +0100

    Update users.js

--- models/users.js
+++ models/users.js
@@ -9,9 +9,9 @@ const UserSchema = new mongoose.Schema({
     instagramUrl: { type: String, required: false },
     intrest: { type: String, required: true },
     funFacts: { type: String, required: true },
-    image: { type: String, required: false },
-    age: { type: String, required: false },
-    gender: { type: String, required: false },
+//     image: { type: String, required: false },
+//     age: { type: String, required: false },
+//     gender: { type: String, required: false },
     active: { type: Boolean, default: false }
 },
     { timestamps: true })
